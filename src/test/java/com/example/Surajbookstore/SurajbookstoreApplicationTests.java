14509bce15afe55827a83159181715b32cfee7ff
Suraj Mishra <mishrasur7@gmail.com>
2022-03-30 15:18:56 +0300

    smoke and weblayer test done

--- src/test/java/com/example/Surajbookstore/SurajbookstoreApplicationTests.java
+++ src/test/java/com/example/Surajbookstore/SurajbookstoreApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.Surajbookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.Surajbookstore.web.BookController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class SurajbookstoreApplicationTests {
+public class SurajbookstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private BookController controller;
 
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }	
 }
